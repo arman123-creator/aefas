feda6a51340aafee558846941997527a3f5ee4a8
arman123-creator <78964558+arman123-creator@users.noreply.github.com>
2022-12-23 18:42:51 +0530

    Add files via upload

---
+++ p105.py
@@ -0,0 +1,36 @@
+import os
+import cv2
+
+
+path = "D:/Users/user/Desktop/P 105/PRO-C105-Project-Images-main/Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+       
+               
+        images.append(file_name)
+        
+
+count = len(images)
+
+frame = cv2.imread(images[0])
+height, width, channels = frame.shape
+size = (width,height)
+
+
+out = cv2.VideoWriter('friend.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 0.8, size)
+
+
+for i in range(count-1,0,-1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+    
+out.release() 
+print("Done")
